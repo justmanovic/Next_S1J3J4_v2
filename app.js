0e2b463ee4cbf2017a683121119b65ff7abf8c7f
jonathan justman <jojo.justman@gmail.com>
2021-09-30 18:51:02 +0200

    ajout des autres persos - remanence à gérer

--- app.js
+++ app.js
@@ -9,15 +9,35 @@ function init() {
   nbPlayers = parseInt(prompt("Combien de joueurs ?"))
   for (i = 1; i <= nbPlayers; i++) {
     let playerName = prompt(`Joueur-${i} quel est ton nom ?`)
-    whichCharacter = parseInt(prompt(`Très bien ${playerName}, quel est ton personnage ? Tapes 1 pour Fighter`))
+    whichCharacter = parseInt(prompt(`Très bien ${playerName}, quel est ton personnage ? Tapes 1 pour Fighter, 2 pour Paladin, 3 pour Monk, 4 pour Berzeker, 5 pour Assassin`))
     switch (whichCharacter) {
       case 1:
         alert(`Tu as choisi d'être un FIGHTER nommé ${playerName}`)
         player = new Fighter(playerName)
         playersArr.push(player)
         break;
+      case 2:
+        alert(`Tu as choisi d'être un PALADIN nommé ${playerName}`)
+        player = new Paladin(playerName)
+        playersArr.push(player)
+        break;
+      case 3:
+        alert(`Tu as choisi d'être un MOINE nommé ${playerName}`)
+        player = new Monk(playerName)
+        playersArr.push(player)
+        break;
+      case 4:
+        alert(`Tu as choisi d'être un BERZEKER nommé ${playerName}`)
+        player = new Berzeker(playerName)
+        playersArr.push(player)
+        break;
+      case 5:
+        alert(`Tu as choisi d'être un ASSASSIN nommé ${playerName}`)
+        player = new Assassin(playerName)
+        playersArr.push(player)
+        break;
       default:
-        alert(`Tant pis pour toi  ${playerName}, Tu seras un Fighter`)
+        alert(`Tant pis pour toi ${playerName}, Tu seras un Fighter`)
         player = new Fighter(playerName)
         playersArr.push(player)
     }
--- player.js
+++ player.js
@@ -18,11 +18,16 @@ class Player {
   }
 
   attackSpecial(victim) {
-    if (this.mana > this.manaCost) {
+    if (this.mana >= this.manaCost) {
       console.log(`Waaaouwww!!! ${this.name} lance son attaque SPECIALE ${this.specialAttackName} et inflige ${this.dmgSpecial} dmg à ${victim.name} `)
       victim.takeDamage(this.dmgSpecial)
       this.checkVictimStatus(victim, this.dmgSpecial)
       this.loseMana(this.manaCost)
+      if (this.healPower !== 0)
+        this.heal(this.healPower)
+      if (this.specialImproveAttack > 0)
+        this.improvedAttack()
+
     } else {
       alert("Vous n'avez pas aseez de mana ! Lancement de l'attaque classique...")
       regularAttack(victim)
@@ -65,18 +70,83 @@ class Player {
     this.mana -= mana
   }
 
+  heal(healPts) {
+    this.hp += healPts
+    healPts > 0 ? console.log(`${this.name} gagne ${healPts} hp grâce à son attaque spéciale !`) : console.log(`${this.name} perd ${healPts} hp à cause de son attaque spéciale!`)
+  }
+
+  improvedAttack() {
+    this.dmg += 1
+    console.log(`Incroyable ! L'attaque de ${this.name} gagne 1 points de dmg ! Elle est maintenant de ${this.dmg} dmg !`)
+  }
 }
 
 class Fighter extends Player {
-  constructor(name, hp = 12, dmg = 4, mana = 40) {
-    super(name, hp, dmg, mana, status)
+  constructor(name, hp, dmg, mana) {
+    super(name)
     this.specialAttackName = 'Dark Vision'
+    this.dmg = 4
+    this.hp = 12
+    this.mana = 40
     this.dmgSpecial = 5
     this.manaCost = 20
-    this.specialAttackArr = []
-    // this.healPower = healPower
+    this.healPower = 0
   }
 }
+
+class Paladin extends Player {
+  constructor(name, hp, dmg, mana) {
+    super(name)
+    this.specialAttackName = 'Healing Lighting'
+    this.dmg = 3
+    this.hp = 16
+    this.mana = 160
+    this.dmgSpecial = 4
+    this.manaCost = 20
+    this.healPower = 5
+  }
+}
+
+class Monk extends Player {
+  constructor(name, hp, dmg, mana) {
+    super(name)
+    this.specialAttackName = 'Heal'
+    this.dmg = 2
+    this.hp = 8
+    this.mana = 200
+    this.dmgSpecial = 0
+    this.manaCost = 25
+    this.healPower = 8
+  }
+}
+
+class Berzeker extends Player {
+  constructor(name, hp, dmg, mana) {
+    super(name)
+    this.specialAttackName = 'Rage'
+    this.dmg = 4
+    this.hp = 8
+    this.mana = 0
+    this.dmgSpecial = 0
+    this.manaCost = 0
+    this.healPower = -1
+    this.specialImproveAttack = 1
+  }
+}
+
+class Assassin extends Player {
+  constructor(name, hp, dmg, mana) {
+    super(name)
+    this.specialAttackName = 'Heal'
+    this.dmg = 6
+    this.hp = 6
+    this.mana = 20
+    this.dmgSpecial = 7
+    this.manaCost = 20
+    this.healPower = 0
+  }
+}
+
 
 
 
